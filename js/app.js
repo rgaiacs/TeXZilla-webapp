c3e437154fc5e59ec83276e22afe2997efaa4218
Raniere Silva <raniere@ime.unicamp.br>
2014-03-10 19:14:19 -0300

    Add automatically update

--- js/app.js
+++ js/app.js
@@ -22,6 +22,34 @@ function update_equation(ev) {
   RM_ENABLE = null;
 }
 
+function preview_equation() {
+    var tex = document.getElementById('tex-input');
+    var notebook = document.getElementById('notebook');
+    var equation;
+    try {
+        equation = TeXZilla.toMathML(tex.value, true, false, true);
+    }
+    catch (error) {
+        equation = document.createElement("p");
+        equation.setAttribute("style", "color:red;");
+        equation.innerHTML = tex.value;
+    }
+    equation.setAttribute('id', 'preview');
+    if (EQUATION) {
+        notebook.insertBefore(equation, EQUATION);
+        notebook.removeChild(EQUATION);
+        var action = document.getElementById('tex-button');
+        action.innerHTML = 'Add';
+        EQUATION = null;
+        RM_ENABLE = true;
+    }
+    else {
+        old_preview = document.getElementById('preview');
+        notebook.insertBefore(equation, old_preview);
+        notebook.removeChild(old_preview);
+    }
+}
+
 function add_equation() {
   var tex = document.getElementById('tex-input');
   if (tex.value === '') {
@@ -31,6 +59,15 @@ function add_equation() {
   var notebook = document.getElementById('notebook');
   var equation = TeXZilla.toMathML(tex.value, true);
   equation.addEventListener('click', update_equation, false);
+  
+  // Remove preview
+  try {
+      var old_preview = document.getElementById('preview');
+      notebook.removeChild(old_preview);
+  }
+  catch (error) {
+  }
+
   if (EQUATION) {
     notebook.insertBefore(equation, EQUATION);
     notebook.removeChild(EQUATION);
@@ -50,6 +87,11 @@ function add_equation() {
   }
   var input = document.getElementById('tex-input');
   input.value = '';
+
+  // Add new preview
+  var new_preview = document.createElement('math');
+  new_preview.setAttribute('id', 'preview');
+  notebook.appendChild(new_preview);
 }
 
 EQUATION = null;
--- js/keyboard-layout.js
+++ js/keyboard-layout.js
@@ -5,17 +5,43 @@ var LAYOUT = {
                 mathml: "<mfrac><mi>x</mi><mi>y</mi></mfrac>",
                 tex: "\\frac{x}{y}"
             },
+            { key: "<math><mo>÷</mo></math>", 
+                mathml: "<mo>÷</mo>", 
+                tex: "\\div"
+            },
+            { key: "<math><msub><mi>x</mi><mi>y</mi></msub></math>", 
+                mathml: "<msub><mi>x</mi><mi>y</mi></msub>",
+                tex: "x_{y}"
+            },
+            { key: "<math><msubsup><mi>x</mi><mi>y</mi><mi>z</mi></msubsup></math>", 
+                mathml: "<msubsup><mi>x</mi><mi>y</mi><mi>z</mi></msubsup>",
+                tex: "x_{y}^{z}"
+            },
+            { key: "<math><msup><mi>x</mi><mi>y</mi></msup></math>", 
+                mathml: "<msup><mi>x</mi><mi>y</mi></msup>",
+                tex: "x^{y}"
+            }
+        ],
+        [
             { key: "<math><msqrt><mi>x</mi></msqrt></math>", 
                 mathml: "<msqrt><mi>x</mi></msqt>",
                 tex: "\\sqrt{x}"
             },
-            { key: "<math><msup><mi>x</mi><mi>y</mi></msup></math>", 
-                mathml: "<msup><mi>x</mi><mi>y</mi></msup>",
-                tex: "x^y"
+            { key: "<math><mroot><mi>x</mi><mi>y</mi></mroot></math>", 
+                mathml: "<mroot><mi>x</mi><mi>y</mi></mroot>",
+                tex: "\\sqrt[y]{x}"
             },
-            { key: "<math><msub><mi>x</mi><mi>y</mi></msub></math>", 
-                mathml: "<msub><mi>x</mi><mi>y</mi></msub>",
-                tex: "x_y"
+            { key: "<math><munder><mi>x</mi><mi>y</mi></munder></math>", 
+                mathml: "<munder><mi>x</mi><mi>y</mi></munder>",
+                tex: "\\underset{y}{x}"
+            },
+            { key: "<math><munderover><mi>x</mi><mi>y</mi><mi>z</mi></munderover></math>", 
+                mathml: "<munderover><mi>x</mi><mi>y</mi><mi>z</mi></munderover>",
+                tex: "\\underset{y}{\\overset{z}{x}}"
+            },
+            { key: "<math><mover><mi>x</mi><mi>y</mi></mover></math>", 
+                mathml: "<mover><mi>x</mi><mi>y</mi></mover>",
+                tex: "\\overset{y}{x}"
             }
         ]
     ]
